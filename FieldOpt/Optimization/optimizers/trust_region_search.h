848f60a1168185b7b442b1e389416971c6ece30f
lingyaw <lingyawang2912@gmail.com>
2017-02-17 13:33:18 +0100

    override vitrual function

--- FieldOpt/Optimization/optimizers/trust_region_search.h
+++ FieldOpt/Optimization/optimizers/trust_region_search.h
@@ -61,7 +61,7 @@ namespace Optimization {
         private:
             void iterate(); //!< Step or contract, perturb, and clear list of recently evaluated cases.
         protected:
-            void handleEvaluatedCase(Case *c) override;
+            void handleEvaluatedCase(Case *c) override {};
 
         };
     }}
