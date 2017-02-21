a7d6e4499fc8e141cc90467d104ae6878872a1fc
lingyaw <lingyawang2912@gmail.com>
2017-02-21 16:37:06 +0100

    add new center point set is_model_complete_ = false;     needs_set_of_points_ = true; need_eval=true;

--- FieldOpt/Optimization/optimizers/trust_region_search.cpp
+++ FieldOpt/Optimization/optimizers/trust_region_search.cpp
@@ -17,19 +17,6 @@ namespace Optimization {
             radius_ = k*radius_;
             polymodel_.setRadius(k);
         }
-        // perturb is not used
-        void TrustRegionSearch::perturb()
-        {
-            QList<Case *> perturbations = QList<Case *>();
-            for (QUuid id : tentative_best_case_->integer_variables().keys())
-                perturbations.append(tentative_best_case_->Perturb(id, Case::SIGN::PLUSMINUS, radius_));
-            for (QUuid id : tentative_best_case_->real_variables().keys())
-                perturbations.append(tentative_best_case_->Perturb(id, Case::SIGN::PLUSMINUS, radius_));
-            for (Case *c : perturbations) {
-                constraint_handler_->SnapCaseToConstraints(c);
-            }
-            case_handler_->AddNewCases(perturbations);
-        }
 
         //tentative_best_case_=base_case(Optimizer(settings,base_case,variable,grid)
         // case_handler_ = new CaseHandler(tentative_best_case_);
@@ -57,17 +44,23 @@ namespace Optimization {
             if (iteration_ == 0) {
                 std:: cout << "Initializing polymodel and completing points" << std::endl;
                 initializeModel();
+                std::cout <<"Is model completed  after initializeModel? "<<polymodel_.isModelReady() << std::endl;
             }
 
             /* Either the PolyModel is ready to give us a derivative, or
              * we need to update points or get cases evaluated
              */
             else if(!polymodel_.isModelReady()) {
-                std:: cout << "Completing model" << std::endl;
+                std:: cout << "Model is not ready, Completing model first" << std::endl;
                 completeModel();
+                std::cout <<"Is model completed (is.ModelReday)? "<<polymodel_.isModelReady() << std::endl;
+
+
+
             }
 
             else {
+                std::cout <<"Is model completed (else)? "<<polymodel_.isModelReady() << std::endl;
                 std::cout << "Model should be ready, we make some opt step " << std::endl;
                 //TODO Here we can use current_model to do an optimization step, which
                 optimizationStep();
@@ -80,21 +73,21 @@ namespace Optimization {
 
         void TrustRegionSearch::initializeModel() {
             // Create polymodel with radius and center and complete set of points.
-            polymodel_ = PolyModel(GetTentativeBestCase(), radius_);
+            polymodel_ = PolyModel(GetTentativeBestCase(), radius_); // is_model_complete=false; need_set_of points=true;
             completeModel();
         }
 
         void TrustRegionSearch::completeModel() {
-            polymodel_.complete_points();
+            polymodel_.complete_points(); // needs_set of points=false after completing points
             // Add cases to case_handler and clear CasesNotEval queue
             case_handler_->AddNewCases(polymodel_.get_cases_not_eval());
-            polymodel_.ClearCasesNotEval();
+            polymodel_.ClearCasesNotEval(); // needs_evals=false
+            polymodel_.set_model_complete(); //is_model_complete=true
             polymodel_.set_evaluations_complete();
         }
 
         void TrustRegionSearch::optimizationStep() // we dont need Perturbation, gradient descent og dog-leg method
-        {
-            polymodel_.calculate_model_coeffs();
+        {   polymodel_.calculate_model_coeffs();
             Eigen::VectorXd optimizationstep;
 
             /* optimizationstep from polymodel for minimum value.
@@ -104,11 +97,11 @@ namespace Optimization {
 
             if (mode_ == Settings::Optimizer::OptimizerMode::Maximize) {
               optimizationstep=-polymodel_.optimizationStep_NG();
-                std::cout << "The optimization step is    " << optimizationstep<< std::endl;
+                std::cout << "Optimizer Mode is Maximize, the optimization step should be   " << optimizationstep<< std::endl;
             }
             else if (mode_ == Settings::Optimizer::OptimizerMode::Minimize) {
                 optimizationstep=polymodel_.optimizationStep_NG();
-                std::cout << "The optimization step is    " << optimizationstep<< std::endl;
+                std::cout << "Optimizer Mode is minimize, The optimization step shoule be  " << optimizationstep<< std::endl;
             }
             // Find point of newCenterPoint which is the best point in current subregion
             // newCenterpoint=current centerpoint+optimization step
@@ -119,6 +112,10 @@ namespace Optimization {
             // Use the best case in current subregion as a new Base Case for next iteration. Creat this Case from its Point
             // tentative_best_case=base_case (center point)
             Case *newBaseCase=polymodel_.CaseFromPoint(NewCenterPoint,tentative_best_case_);
+            polymodel_.addCenterPoint(NewCenterPoint); //needs_set of points=true, is model_complete=false;
+            std::cout <<"Is model completed after add CenterPoint? "<<polymodel_.isModelReady() << std::endl;
+            scaleRadius(0.5);
+            tentative_best_case_=newBaseCase;
 
             //polymodel_.optimizationStep();
             //Case* a =polymodel_.find_NewBaseCase();
@@ -141,9 +138,7 @@ namespace Optimization {
              */
 
 
-            polymodel_.addCenterPoint(NewCenterPoint);
-            scaleRadius(0.5);
-            tentative_best_case_=newBaseCase;
+
 
 
         }
